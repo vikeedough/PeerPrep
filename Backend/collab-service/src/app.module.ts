f09e29d9a2202f38c898f6b90ed9d74130c23cd5
vikeedough <156529589+vikeedough@users.noreply.github.com>
2025-10-09 23:05:28 +0800

    KAN-6 Add @nestjs/config for environment configuration and enable CORS support

--- Backend/collab-service/src/app.module.ts
+++ Backend/collab-service/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CollabGateway } from './collab/collab.gateway';
 import { CollabModule } from './collab/collab.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CollabModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CollabModule,
+  ],
   controllers: [AppController],
   providers: [AppService, CollabGateway],
 })
