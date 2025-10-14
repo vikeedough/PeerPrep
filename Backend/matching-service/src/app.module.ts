7c2f8dfa6c43d23ba632a18f8aa34a4961cde176
AnWei <e1122931@u.nus.edu>
2025-10-14 23:11:33 +0800

    Add basic matching based on topics and difficulty

--- Backend/matching-service/src/app.module.ts
+++ Backend/matching-service/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MatchingGateway } from './matching/matching.gateway';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, MatchingGateway],
+  providers: [MatchingGateway],
 })
 export class AppModule {}
